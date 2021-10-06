0f914dc5ab75cf2d2515d417501e4f3a2cb57101
Philippe Ngom <philngom@gmail.com>
2021-10-06 14:01:38 -0700

    added change color buttons and added funtionality

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const updateButton = document.getElementById('update-button');
 const nameInput = document.getElementById('name-input');
 const nameDisplay = document.getElementById('name-display');
+const greenButton = document.getElementById("green-button");
+const blueButton = document.getElementById("blue-button");
+const pinkButton = document.getElementById("pink-button");
+const tag = document.getElementById("tag");
 
 updateButton.addEventListener('click', () => {
     const newName = nameInput.value;
     nameDisplay.textContent = newName;
   console.log(nameInput.value);
 });
+
+greenButton.addEventListener('click', () => {
+  tag.style.backgroundColor = "green";
+})
+
+blueButton.addEventListener('click', () => {
+  tag.style.backgroundColor = "blue";
+})
+
+pinkButton.addEventListener('click', () => {
+  tag.style.backgroundColor = "pink";
+})
